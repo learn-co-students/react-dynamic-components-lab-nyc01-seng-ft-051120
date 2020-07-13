23c45c900cff3f6bbbea766bf5d66befc8ba0895
JulianCedric <julian.pormentilla@gmail.com>
2020-07-13 13:10:18 -0400

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 
-export default class ColorBox extends Component {
+class ColorBox extends Component {
 
-  state = {
-    todos: [
+  // state = {
+  //   todos: [
       
-    ]
-  }
+  //   ]
+  // }
+
+
+
+
+
+  // render() {
+  //   return (
+  //     <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
+  //       {/* your conditional code here! */}
+
+  //       {/* ternary conditional: */}
+
+  //         {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1} /> : null }
+
+  //     </div>
+  //   )
+  // }
 
   render() {
-    return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
-      </div>
-    )
+    if(this.props.opacity >= 0.2) {
+      return (
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+          <ColorBox opacity={this.props.opacity - 0.1} />
+        </div>
+      )
+    } else {
+      return null
+    }
   }
 
 }
 
+export default ColorBox 
--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,20 @@
 //your code here
+
+// here, Comment is a JavaScript Object that renders a 'virtual' react element (as its return value / output), which maps to an actual DOM element 
+
+import React, { Component } from 'react'
+
+class Comment extends Component {
+
+    render() {
+
+        return (
+            <div className="comment">
+                {this.props.commentText}
+            </div>
+        ) 
+    } 
+
+}
+
+export default Comment 
